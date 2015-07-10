a7a71c807ba862c1b02d8ac9ffdb487836c0932a
Vadim Brodsky <vadimb@crankworks.ca>
2015-07-10 11:51:44 -0400

    Add PostCss autoprefixer and watch example

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,46 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    postcss: {
+      options: {
+        map: true, // inline sourcemaps
+
+        // or
+        map: {
+            inline: false, // save all sourcemaps as separate files...
+            annotation: 'build/css' // ...to the specified directory
+        },
+
+        processors: [
+          require('pixrem')(), // add fallbacks for rem units
+          require('autoprefixer-core')({
+            // add vendor prefixes
+            // https://github.com/ai/browserslist#queries
+            // browsers: 'last 2 versions'
+            // browsers: 'last 5 versions'
+            // browsers: '> 5%'
+            // browsers: 'IE 8'
+            browsers: '> 1%, last 2 versions, Firefox ESR, Opera 12.1'
+          }),
+          // require('cssnano')() // minify the result
+        ]
+      },
+      dist: {
+        files: {
+          'build/css/style.css': 'src/sass/style.css'
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['src/sass/style.css'],
+        tasks: ['postcss']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
   grunt.registerTask('speak', function() {
     console.log("I'm speaking");
   });
